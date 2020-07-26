6519d7d53970a21a4fa4a0eda106f07fb0937052
Pwn <phyowainyunt@ucsy.edu.mm>
2020-07-26 20:39:01 +0630

    stock done

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 const express = require('express');
 const app = express();
-// const path = require('path');
-var exphbs = require('express-handlebars');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function api_call(finishedAPI, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_f1ce8aa6d2fb475c826b41edbfa4cb26', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        if (res.statusCode === 200) { finishedAPI(body) }
+    });
+}
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-const otherStuff = "This is another stuff";
 
 app.get('/', function(req, res) {
-    res.render('home', {
-        stuff: otherStuff
-    });
+    api_call(function(doneAPI) {
+        res.render('home', {
+            stock: doneAPI
+        });
+    }, "fb");
 });
 
-// app.use(express.static(path.join(__dirname, 'public')));
-app.get('/index', function(req, res) {
-    res.render('index');
+app.post('/', function(req, res) {
+    api_call(function(doneAPI) {
+        // post_stock = req.body.stock_finder
+        res.render('home', {
+            stock: doneAPI,
+        });
+    }, req.body.stock_finder);
 });
 
 app.get('/about', function(req, res) {
-    res.render('about');
+    res.render('about')
 })
+
+app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/index', function(req, res) {
+//     res.render('index');
+// });
+
+// app.get('/about', function(req, res) {
+//     res.render('about');
+// })
 
 
 app.listen(PORT, () => console.log("Server listening on port " + PORT));
